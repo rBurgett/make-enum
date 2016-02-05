0569077c438a4402d2285b8075175b501df9ff66
Ryan <ryan@burgettweb.net>
2016-02-04 21:21:46 -0500

    Added initial tests

--- test/tests.js
+++ test/tests.js
@@ -5,5 +5,23 @@ import should from 'should';
 import makeEnum from '../lib/index'
 
 describe('makeEnum', () => {
-    makeEnum.should.be.ok;
+
+    it('should be a function', () => makeEnum.should.be.a.Function());
+    it('should return an object', () => makeEnum().should.be.an.Object());
+
+    describe('the returned object', () => {
+
+        it('should contain a matching key/value pair for each param sent in', () => {
+
+            const enumishObj = makeEnum('red', 'green', 'blue');
+
+            const isValid = Object.keys(enumishObj).reduce((b, v) => {
+                return b = (enumishObj[v] === v) ? b : false;;
+            }, true);
+
+            isValid.should.be.true();
+
+        });
+
+    });
 });
